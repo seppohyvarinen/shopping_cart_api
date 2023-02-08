a9485a07c34996efd5b632bb51d0c34c4be45877
seppohyvarinen <seppo.hyvarinen@tuni.fi>
2023-02-08 15:12:15 +0200

    Add: jsonise response

--- routes.js
+++ routes.js
@@ -4,10 +4,10 @@ let shoppingCart = express.Router();
 
 shoppingCart.get("/", async (req, res) => {
   try {
-    let tervis = "tervis kuis pyyhkis";
-
+    let tervis = { message: "tervis kuis pyyhkis", message2: "fine thanks" };
+    let jsoned = JSON.stringify(tervis);
     res.statusCode = 200;
-    res.send(tervis);
+    res.send(jsoned);
   } catch (error) {
     res.statusCode = 404;
     res.send(error);
