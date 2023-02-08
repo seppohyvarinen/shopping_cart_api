9018bc78fccc31d8cc10f92f98c6dbc7cc10a5f9
seppohyvarinen <seppo.hyvarinen@tuni.fi>
2023-02-08 15:17:48 +0200

    Add: arrayize the json

--- routes.js
+++ routes.js
@@ -4,7 +4,7 @@ let shoppingCart = express.Router();
 
 shoppingCart.get("/", async (req, res) => {
   try {
-    let tervis = { message: "tervis kuis pyyhkis", message2: "fine thanks" };
+    let tervis = [{ message: "tervis kuis pyyhkis", message2: "fine thanks" }];
     let jsoned = JSON.stringify(tervis);
     res.statusCode = 200;
     res.send(jsoned);
